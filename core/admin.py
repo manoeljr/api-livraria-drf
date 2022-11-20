6a38b8ee0f7908d157f906f0afc44b6ae1aa514a
manoeljr <manoelv8@gmail.com>
2022-11-19 21:10:21 -0300

    Adicionando mais alguns modulos e models

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
+from core.models import Autor
 from core.models import Categoria
 from core.models import Editora
+from core.models import Livro
+from core.models import Compra
+
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
+admin.site.register(Compra)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=255)
@@ -14,3 +16,40 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'autores'
+
+    def __str__(self):
+        return self.nome
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.FloatField()
+
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros')
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros')
+    autores = models.ManyToManyField(Autor, related_name='livros')
+
+    def __str__(self):
+        return f'{self.titulo} ({self.editora})'
+
+
+class Compra(models.Model):
+
+    class StatusCompra(models.IntegerChoices):
+        CARINHO = 1, 'Carinho'
+        REALIZADO = 2, 'Realizado'
+        PAGO = 3, 'Pago'
+        ENTREGUE = 4, 'Entregue'
+
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name='compras')
+    status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARINHO)
+    
